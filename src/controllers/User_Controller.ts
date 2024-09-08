8001de580e1dbf4832b7f8ced6b73434c9527b3d
trunglevie0319 <trunglevie0319@gmail.com>
2024-09-08 16:29:13 +0700

    Second commit: build service and controller layers for table User

--- src/controllers/User_Controller.ts
+++ src/controllers/User_Controller.ts
@@ -56,3 +56,124 @@
 //     }
 //   }
 // }
+
+import { Request, Response } from "express";
+import UserService from "../services/User_Service";
+
+export class UserController {
+  static registerUser = async (
+    req: Request, res: Response
+  ) => {
+    const {
+      username, password, email
+    } = req.body;
+    try {
+      const newUser = await UserService.registerUser(
+        username, password, email
+      );
+      if (!newUser) throw new Error("Unexpected exception")
+      else if (newUser instanceof Error) throw newUser;
+      else res.status(201).json(newUser);
+    } catch (error) {
+      res.status(400).json({ error: `Error registering user: ${new Object(error).toString()}` });
+    }
+  };
+
+  static loginUser = async (
+    req: Request, res: Response
+  ) => {
+    const { username, password } = req.body;
+    try {
+      const result = await UserService.loginUser(username, password);
+      if (result instanceof Error) throw result;
+      else res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: `Error logging user in: ${new Object(error).toString()}` });
+    }
+  };
+
+  static logoutUser = async (
+    req: Request, res: Response
+  ) => {
+    const userId = req.user?.userId || "";
+    try {
+      if (!userId) throw new Error("User not found");
+      const result = await UserService.logoutUser(userId);
+      if (result instanceof Error) throw result;
+      else res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: `Error logging user out: ${new Object(error).toString()}` });
+    }
+  };
+
+  static refreshToken = async (
+    req: Request, res: Response
+  ) => {
+    const { refreshToken } = req.body;
+    try {
+      const result = await UserService.refreshToken(refreshToken);
+      if (result instanceof Error) throw result;
+      else res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: `Error refreshing token: ${new Object(error).toString()}` });
+    }
+  };
+
+  static findUserById = async (
+    req: Request, res: Response
+  ) => {
+    const userId = req.params.userId;
+    try {
+      if (!userId) throw new Error("UserId is empty");
+      const result = await UserService.findUserById(userId);
+      if (result instanceof Error) throw result;
+      else res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: `Error fetching user by id: ${new Object(error).toString()}` });
+    }
+  };
+
+  static findUserByUsername = async (
+    req: Request, res: Response
+  ) => {
+    const username = req.params.username;
+    try {
+      if (!username) throw new Error("Input username is empty");
+      const result = await UserService.findUserByUsername(username);
+      if (result instanceof Error) throw result;
+      else res.status(200).json(result);      
+    } catch (error) {
+      res.status(400).json({ error: `Error fetching user by username: ${new Object(error).toString()}` });
+    }
+  };
+
+  static findLockedUsers = async (
+    req: Request, res: Response
+  ) => {
+    try {
+      const result = await UserService.findLockedUsers();
+      if (result instanceof Error) throw result;
+      else res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: `Error fetching locked users: ${new Object(error).toString()}` });
+    }
+  };
+
+  static findLockedUserById;
+
+  static findLockedUserByUsername;
+
+  static findAllUsers;
+  
+  static findAllUserById;
+
+  static findAllUserByUsername;
+
+  static lockUser;
+
+  static unlockUser;
+
+  static updatePassword;
+}
+
+export default UserController;
--- src/services/User_Service.ts
+++ src/services/User_Service.ts
@@ -114,21 +114,224 @@ const JWT_REFRESH_EXPIRES_IN = '7d'; // Long-lived refresh token
 
 class UserService {
   // GET
-  static getUsers; // get all users
-  static getUserById; // get user via user_id
-  static getUserByUsername; // get user via username
-  static getUserByEmail; // get user via email
-  static getPasswordById; // get user's password via user_id, for changing password task
+  static findUsers;
+
+  static findUserById = async (userId: string) => {
+    return await User.findOne({
+      where: {
+        user_id: {
+          [Op.eq]: userId
+        },
+        is_locked: {
+          [Op.eq]: false
+        }
+      }
+    })
+  };
+
+  static findUserByUsername = async (username: string) => {
+    return await User.findAll({
+      where: {
+        username: { [Op.like]: "%".concat(username).concat("%") },
+        is_locked: { [Op.eq]: false }
+      }
+    });
+  };
+  
+  static findUserByEmail;
+  
+  static findLockedUsers = async () => {
+    return await User.findAll({
+      where: {
+        is_locked: { [Op.eq]: true }
+      }
+    });
+  };
+  
+  static findLockedUserById = async (userId: string) => {
+    return await User.findOne({
+      where: {
+        user_id: { [Op.eq]: userId },
+        is_locked: { [Op.eq]: true }
+      }
+    });
+  };
+  
+  static findLockedUserByUsername = async (username: string) => {
+    return await User.findAll({
+      where: {
+        username: { [Op.like]: "%".concat(username).concat("%") },
+        is_locked: { [Op.eq]: true }
+      }
+    });
+  };
+  
+  static findLockedUserByEmail;
+
+  static findAllUsers = async () => {
+    return await User.findAll();
+  };
+
+  static findAllUserById = async (userId: string) => {
+    return await User.findByPk(userId);
+  };
+
+  static findAllUserByUsername = async (username: string) => {
+    return await User.findAll({
+      where: {
+        username: { [Op.like]: "%".concat(username).concat("%") }
+      }
+    });
+  };
+
+  static findAllUserByEmail;
+  
   // POST
-  static registerUser; // create new user
-  static loginUser; 
-  static refreshToken; // refresh user's refreshToken
-  static logoutUser; 
-  static unlockAccount; // unlock user's account from being lock by login failed
+  static registerUser = async (
+    username: string, password: string, email: string
+  ) => {
+    const password_hash = await bcrypt.hash(password, SALT_ROUNDS);
+    const newUser = await User.create({
+      username, password_hash, email
+    });
+    return newUser;
+  };
+
+  static loginUser = async (
+    username: string, password: string
+  ) => {
+    const user = await User.findOne({ where: { username } });
+    if (!user) return new Error("User not found");
+    if (user.is_locked && user.unlock_timestamp && user.unlock_timestamp > new Date()) {
+      return new Error("User is locked");
+    }
+    
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password_hash
+    );
+    if (!isMatch) {
+      user.login_attempts += 1;
+      if (user.login_attempts >= 5) {
+        user.is_locked = true;
+        user.unlock_timestamp = new Date(Date.now() + 5 * 60 * 1000);
+        await user.save();
+        return new Error("Invalid password");
+      }
+    }
+
+    user.login_attempts = 0;
+    user.last_login = new Date();
+    await user.save();
+
+    const payload = {
+      userId: user.user_id,
+      role: user.role
+    };
+    const accessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+    const refreshToken = jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn: JWT_REFRESH_EXPIRES_IN });
+
+    user.refresh_token = refreshToken;
+    await user.save();
+    return {
+      user, accessToken, refreshToken
+    };
+  };
+
+  static refreshToken = async (
+    oldToken: string
+  ) => {
+    try {
+      const decoded = jwt.verify(oldToken, JWT_REFRESH_SECRET) as JwtPayload;
+      const user = await User.findOne({
+        where: {
+          user_id: {
+            [Op.eq]: decoded.userId
+          },
+          refresh_token: {
+            [Op.eq]: oldToken
+          }
+        }
+      });
+      if (!user) return new Error("User not found or invalid refresh token");
+
+      const payload = {
+        userId: user.user_id,
+        role: user.role
+      }
+      const newAccessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+      const newRefreshToken = jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn: JWT_REFRESH_EXPIRES_IN });
+      user.refresh_token = newRefreshToken;
+      await user.save();
+      return {
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken
+      };
+    } catch (error) {
+      return new Error(`Invalid refresh token: ${new Object(error).toString()}`);
+    }
+  };
+
+  static logoutUser = async (
+    userId: string
+  ) => {
+    const user = await User.findByPk(userId);
+    if (!user) return new Error("User not found");
+    user.refresh_token = null;
+    await user.save();
+    return {};
+  };
+
   // PATCH
-  static updatePassword; // change the password of user's account
-  // DELETE
-  static deleteUser; // delete user
+  static lockUser = async (userId: string, lockDurationMilliseconds: number) => {
+    const user = await this.findUserById(userId);
+    if (!user) return new Error("User not found");
+    user.is_locked = true;
+    user.unlock_timestamp = new Date(Date.now() + lockDurationMilliseconds);
+    user.login_attempts = 100;
+    await user.save();
+    return user;
+  };
+
+  static unlockUser = async (userId: string) => {
+    const user = await this.findLockedUserById(userId);
+    if (!user) return new Error("User not found");
+    
+    user.is_locked = false;
+    user.unlock_timestamp = null;
+    user.login_attempts = 0;
+    await user.save();
+    return user;
+  };
+
+  static updateUser;
+
+  static updatePassword = async (
+    userId: string,
+    updatedData: {
+      oldPassword: string,
+      newPassword: string
+    }
+  ) => {
+    const user = await this.findUserById(userId);
+    if (!user) return new Error("User not found");
+
+    const isMatch = await bcrypt.compare(
+      updatedData.oldPassword, user.password_hash
+    );
+    if (!isMatch) {
+      user.login_attempts += 1;
+      if (user.login_attempts >= 5) {
+        user.is_locked = true;
+        user.unlock_timestamp = new Date(Date.now() + 5 * 60 * 1000);
+        await user.save();
+        return new Error("Invalid password");
+      }
+    }
+    user.password_hash = await bcrypt.hash(updatedData.newPassword, SALT_ROUNDS);
+    await user.save();
+    return user;
+  };
 }
 
 export default UserService;
